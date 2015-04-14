098dbc751a59f36ecdc37f8ef5f7eb5de684f0d9
Jonathan Stepp-netbook <jnthn.stpp@gmail.com>
2015-04-14 19:53:56 -0400

    Initial irr demo, it works, but does not have a high precision, may change libraries

--- honors/irr.js
+++ honors/irr.js
@@ -1,19 +1,24 @@
-var time = 0;
-var per = 2;
+var ans = new BigNumber(0);
+
+var rootTwo = function(x){
+    if(x === 0){
+	return new BigNumber(2);
+    } else {
+	var ret = new BigNumber(2);
+	return ret.plus(new BigNumber(1).div(rootTwo(x-1)));
+    }
+};
 
 var irrAction = function(){
     var field = document.getElementById("irr-field");
-    per = field.value;
-}
+    ans = rootTwo(field.value).minus(new BigNumber(1));
+};
 var irr = function(ctx){
     //called every loop
-    time += 1;
     
     ctx.clearRect(0, 0, 300, 300);
     ctx.fillStyle = "green";
-    var apt = 50;
-    var locx = 150 + 100 * Math.cos((2*Math.PI/ (60 * per))* time);
-    var locy = 150 + 100 * Math.sin((2*Math.PI/ (60 * per))* time);
-    ctx.fillRect(locy - apt, locx - apt, 100, 100);
 
-}
+    ctx.font = "48px, Serif";
+    ctx.fillText(ans.toFormat(50), 10, 10);
+};
