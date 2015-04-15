8a61731d81cb12510d98edfb181319d14b7160fe
Jonathan Stepp-netbook <jnthn.stpp@gmail.com>
2015-04-15 07:10:53 -0400

    Example works well, have not tested how long it will run for.
    TODO:
    Add a graphical user interface
    Change to work with numbers other than sqrt(2)
    hardcode the precision

--- honors/irr.js
+++ honors/irr.js
@@ -1,4 +1,7 @@
+var num = new BigNumber(0);
 var ans = new BigNumber(0);
+var div = new BigNumber(1);
+var steps = 0;
 
 var rootTwo = function(x){
     if(x === 0){
@@ -11,14 +14,44 @@ var rootTwo = function(x){
 
 var irrAction = function(){
     var field = document.getElementById("irr-field");
-    ans = rootTwo(field.value).minus(new BigNumber(1));
+    num = rootTwo(field.value).minus(new BigNumber(1));
 };
+
+var step = function(){
+    if(div.gt(ans)){
+	ans = ans.plus(num);
+    }
+    
+    ans = ans.minus(div);
+
+    steps += 1;
+    
+    
+};
+
 var irr = function(ctx){
     //called every loop
     
     ctx.clearRect(0, 0, 300, 300);
     ctx.fillStyle = "green";
 
-    ctx.font = "48px, Serif";
-    ctx.fillText(ans.toFormat(50), 10, 10);
+    ctx.font = "48px Serif";
+    ctx.fillText(num.toFormat(8), 5, 50);
+    ctx.fillStyle = "red";
+    ctx.fillText(ans.toFormat(8), 5, 100);
+    ctx.fillStyle = "blue";
+    ctx.fillText(steps, 5, 150);
+};
+
+var run = function(){
+
+    var canvas = document.getElementById("irr-canvas");
+    var ctx = canvas.getContext("2d");
+    
+    if(!(ans.equals(new BigNumber(0)))){
+	step();
+	irr(ctx);
+    }
+    window.requestAnimationFrame(run);
+    
 };
